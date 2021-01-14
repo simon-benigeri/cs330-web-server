91f423941d0a8d89db1f6b5b74853843ef3ca805
simon-benigeri <simon.benigeri@gmail.com>
2021-01-13 18:09:11 -0600

    My first commit' :wq

---
+++ db.py
@@ -0,0 +1,12 @@
+import os
+from flask_mongoengine import MongoEngine
+
+def init_database_connection(app):
+    app.config['MONGODB_SETTINGS'] = {
+        'db': os.environ.get('DATABASE_NAME'),
+        'host': 'mongodb+srv://' + os.environ.get('HOST') + '/' + os.environ.get('DATABASE_NAME') + '?retryWrites=true&w=majority',
+        'username': os.environ.get('USERNAME'),
+        'password': os.environ.get('PASSWORD')
+    }
+    db = MongoEngine()
+    db.init_app(app)
