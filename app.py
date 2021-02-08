b2d9f5824f1731c18d6653781dc089da961d78b9
simon-benigeri <simon.benigeri@gmail.com>
2021-02-08 09:53:05 -0600

    Adding Posts Front End

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ flask run
 from flask_restful import Api
 from flask import Flask, request, Response
 from flask_cors import CORS
+from flask import render_template
 
 import db
 from views import posts, comments
@@ -16,6 +17,26 @@ CORS(app)
 db.init_database_connection(app)
 api = Api(app)
 
+########################### New in HW02
+@app.route('/')
+def list_posts():
+    return render_template('get-posts.html')
+
+@app.route('/add-post/')
+def create_post():
+    return render_template('create-post.html')
+
+@app.route('/post/')
+def get_single_post():
+    return render_template('post-detail.html')
+########################### End New in HW02
+
+
+# routes from other files:
+posts.initialize_routes(api)
+comments.initialize_routes(api)
+
+"""
 # connect your routes to your app:
 @app.route('/')
 def home_page():
@@ -24,7 +45,7 @@ def home_page():
 # routes from other files:
 posts.initialize_routes(api)
 comments.initialize_routes(api)
-
+"""
 
 
 if __name__ == "__main__":
